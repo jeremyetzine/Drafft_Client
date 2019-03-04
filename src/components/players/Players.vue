afffa385fa93dbad16246247dc96f10b1372b220
JETZ <jeremyetzine@hotmail.com>
2019-03-04 12:52:59 +1100

    user auth working

---
+++ src/components/players/Players.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="players">
+    <h3> Players </h3>
+    <div class="sign-out float-right">
+      <label @click="signOut">Sign out</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    signOut () {
+      this.$http.secured.delete('/signin')
+        .then(response => {
+          delete localStorage.csrf
+          delete localStorage.signedIn
+          this.$router.replace('/')
+        })
+        .catch(error => this.setError(error, 'Cannot sign out'))
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+import Players from '@/components/players/Players'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/players',
+      name: 'Players',
+      component: Players
+    }
+  ]
+})
