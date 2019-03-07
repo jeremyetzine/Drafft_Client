67ea66fcba92f05f780cb6e2c8b6eaa3e6e149cf
JETZ <jeremyetzine@hotmail.com>
2019-03-08 10:15:23 +1100

    added jordan face

--- src/components/Squad.vue
+++ src/components/Squad.vue
@@ -1,70 +1,76 @@
 <template lang="html">
   <div class="container">
-    <h1>the {{squads[currentUserID-1].name}}</h1>
-    <div>
-      <router-link to="/draft">back to the draft</router-link>
+    <div class="background">
+      <h1>the {{squads[currentUserID-1].name}}</h1>
+      <div>
+        <router-link to="/draft">back to the draft</router-link>
+      </div>
+      <table class="available">
+        <tr>
+          <th>Name</th>
+          <th>Position</th>
+          <th>PTS</th>
+          <th>RB</th>
+          <th>AST</th>
+          <th>STL</th>
+          <th>BLK</th>
+          <th>FG%</th>
+          <th>FT%</th>
+          <th>3PM</th>
+          <th>3P%</th>
+          <th>Allstar Status</th>
+        </tr>
+        <tr v-for="player in squads[currentUserID-1].players">
+          <td>{{player.name}}</td>
+          <td>{{player.position}}</td>
+          <td>{{player.pts}}</td>
+          <td>{{player.rb}}</td>
+          <td>{{player.ast}}</td>
+          <td>{{player.stl}}</td>
+          <td>{{player.blk}}</td>
+          <td>{{player.fgperc}}%</td>
+          <td>{{player.ftperc}}%</td>
+          <td>{{player.threepm}}</td>
+          <td>{{player.threeperc}}%</td>
+          <td>{{player.allstar}}</td>
+        </tr>
+      </table>
+      <marquee scrollamount="10">
+        <i class="fas fa-basketball-ball ball10"></i>
+      </marquee>
+      <marquee scrollamount="3">
+        <i class="fas fa-basketball-ball ball3"></i>
+      </marquee>
+      <marquee scrollamount="8">
+        <i class="fas fa-basketball-ball ball8"></i>
+      </marquee>
+      <marquee scrollamount="6">
+        <i class="fas fa-basketball-ball ball6"></i>
+      </marquee>
+      <marquee scrollamount="1">
+        <i class="fas fa-basketball-ball ball1"></i>
+      </marquee>
+      <marquee scrollamount="7">
+        <i class="fas fa-basketball-ball ball7"></i>
+      </marquee>
+      <marquee scrollamount="2">
+        <i class="fas fa-basketball-ball ball2"></i>
+      </marquee>
+      <marquee scrollamount="4">
+        <i class="fas fa-basketball-ball ball4"></i>
+      </marquee>
+      <marquee scrollamount="9">
+        <i class="fas fa-basketball-ball ball9"></i>
+      </marquee>
+      <marquee scrollamount="5">
+        <i class="fas fa-basketball-ball ball5"></i>
+      </marquee>
+      <!-- <marquee scrollamount="5">
+        <div class="sad">
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Michael_Jordan_crying.jpg/220px-Michael_Jordan_crying.jpg" alt="cry">
+        </div>
+      </marquee> -->
     </div>
-    <table class="available">
-      <tr>
-        <th>Name</th>
-        <th>Position</th>
-        <th>PTS</th>
-        <th>RB</th>
-        <th>AST</th>
-        <th>STL</th>
-        <th>BLK</th>
-        <th>FG%</th>
-        <th>FT%</th>
-        <th>3PM</th>
-        <th>3P%</th>
-        <th>Allstar Status</th>
-      </tr>
-      <tr v-for="player in squads[currentUserID-1].players">
-        <td>{{player.name}}</td>
-        <td>{{player.position}}</td>
-        <td>{{player.pts}}</td>
-        <td>{{player.rb}}</td>
-        <td>{{player.ast}}</td>
-        <td>{{player.stl}}</td>
-        <td>{{player.blk}}</td>
-        <td>{{player.fgperc}}%</td>
-        <td>{{player.ftperc}}%</td>
-        <td>{{player.threepm}}</td>
-        <td>{{player.threeperc}}%</td>
-        <td>{{player.allstar}}</td>
-      </tr>
-    </table>
-    <marquee scrollamount="10">
-      <i class="fas fa-basketball-ball ball10"></i>
-    </marquee>
-    <marquee scrollamount="3">
-      <i class="fas fa-basketball-ball ball3"></i>
-    </marquee>
-    <marquee scrollamount="8">
-      <i class="fas fa-basketball-ball ball8"></i>
-    </marquee>
-    <marquee scrollamount="6">
-      <i class="fas fa-basketball-ball ball6"></i>
-    </marquee>
-    <marquee scrollamount="1">
-      <i class="fas fa-basketball-ball ball1"></i>
-    </marquee>
-    <marquee scrollamount="7">
-      <i class="fas fa-basketball-ball ball7"></i>
-    </marquee>
-    <marquee scrollamount="2">
-      <i class="fas fa-basketball-ball ball2"></i>
-    </marquee>
-    <marquee scrollamount="4">
-      <i class="fas fa-basketball-ball ball4"></i>
-    </marquee>
-    <marquee scrollamount="9">
-      <i class="fas fa-basketball-ball ball9"></i>
-    </marquee>
-    <marquee scrollamount="5">
-      <i class="fas fa-basketball-ball ball5"></i>
-    </marquee>
-
   </div>
 </template>
 
@@ -129,6 +135,13 @@ export default {
 </script>
 
 <style>
+  .background {
+    background-image: url(../assets/cryingjordan.jpg);
+    color: white;
+  }
+  .sad {
+    width: 100px;
+  }
   .available {
     margin: auto;
   }
@@ -141,33 +154,43 @@ export default {
   }
   .ball1 {
     -webkit-animation: rotation 9s infinite linear;
+    z-index: 10;
   }
   .ball2 {
     -webkit-animation: rotation 5s infinite linear;
+    z-index: 10;
   }
   .ball3 {
     -webkit-animation: rotation 5s infinite linear;
+    z-index: 10;
   }
   .ball4 {
     -webkit-animation: rotation 3s infinite linear;
+    z-index: 10;
   }
   .ball5 {
     -webkit-animation: rotation 3s infinite linear;
+    z-index: 10;
   }
   .ball6 {
     -webkit-animation: rotation 3s infinite linear;
+    z-index: 10;
   }
   .ball7 {
     -webkit-animation: rotation 2s infinite linear;
+    z-index: 10;
   }
   .ball8 {
     -webkit-animation: rotation 2s infinite linear;
+    z-index: 10;
   }
   .ball9 {
     -webkit-animation: rotation 2s infinite linear;
+    z-index: 10;
   }
   .ball10 {
     -webkit-animation: rotation 2s infinite linear;
+    z-index: 10;
   }
   @-webkit-keyframes rotation {from {-webkit-transform: rotate(359deg)} to {-webkit-transform: rotate(0deg)}}
 </style>
